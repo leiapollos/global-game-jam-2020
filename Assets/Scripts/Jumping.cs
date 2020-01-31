674e54b44deca02c6a3e17a792675b3b20809f70
leiapollos <andreleite98@hotmail.com>
2020-01-31 23:33:18 +0000

    player actions

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : PlayerAction
+{
+    Jumping(Player _player)
+    {
+        player = _player;
+    }
+
+    public override void DoAction()
+    {
+        base.DoAction();
+        //JUMP
+    }
+}
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Action : MonoBehaviour
+public abstract class PlayerAction
 {
+    protected Player player;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public virtual void DoAction() { }
 }
