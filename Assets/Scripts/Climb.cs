1422ce32927d89bb26c44a8ab88b6eb051400aed
leiapollos <andreleite98@hotmail.com>
2020-02-01 01:13:24 +0000

    background + climbing

---
+++ Assets/Scripts/Climb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climb : PlayerAction
+{
+    public Climb(Player _player) : base(_player)
+    {
+    }
+
+    public override void DoAction()
+    {
+        base.DoAction();
+        //climb
+        Vector3 pos = player.transform.position;
+        pos.y += 1.0f;
+        player.transform.position = pos;
+        player.action = new ClimbIdle(player);
+    }
+}
