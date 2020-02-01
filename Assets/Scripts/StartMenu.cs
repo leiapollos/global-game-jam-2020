673f125788748a25a881023f15000eabc0d4cfff
leiapollos <andreleite98@hotmail.com>
2020-02-01 16:11:54 +0000

    small fix

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
