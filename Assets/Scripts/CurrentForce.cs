fa9708b31ffec8521ddb33917c1e444632a4f921
GodVenn <torstein-baltazar@hotmail.com>
2020-02-01 18:48:02 +0000

    Water Labyrinth

---
+++ Assets/Scripts/CurrentForce.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentForce : MonoBehaviour
+{
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    };
+
+    static private Dictionary<Direction, Vector2> directions = new Dictionary<Direction, Vector2> {
+        { Direction.Down, Vector2.down },
+        { Direction.Up, Vector2.up },
+        { Direction.Left, Vector2.left },
+        { Direction.Right, Vector2.right },
+    };
+
+    public Direction intendedDir;
+    public float currentStrength;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+            rb.gravityScale = 0;
+
+            Debug.Log("oof");
+            rb.MovePosition(rb.position + directions[intendedDir] * currentStrength * Time.deltaTime);
+        }
+    }
+}
