5b08250914a5d401c5346074b305e40d975cf8d4
leiapollos <andreleite98@hotmail.com>
2020-02-01 02:55:48 +0000

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(this.player.transform.position.x, this.player.transform.position.y, this.transform.position.z); 
+        this.transform.position = pos;
+    }
+}
