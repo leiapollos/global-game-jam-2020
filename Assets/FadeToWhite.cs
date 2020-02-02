5a27b227f416e202ec991c26d627832a4cf72ada
joaopat98 <joaopat98@gmail.com>
2020-02-02 12:54:35 +0000

    fade to white

---
+++ Assets/FadeToWhite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeToWhite : MonoBehaviour
+{
+    public SpriteRenderer spr;
+    public float FadeTime = 5f;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StartCoroutine(Fade());
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        float t = 0;
+        while (t < FadeTime)
+        {
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime;
+            Color c = spr.color;
+            c.a = EasingFunction.EaseInOutCubic(0, 1, t / FadeTime);
+            spr.color = c;
+        }
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("StartMenu");
+    }
+}
